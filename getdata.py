01958d182d64bb50333ec1e48646a8cae5688417
Rahima <rahimasiddiqui6@gmail.com>
2023-06-08 09:44:38 +0500

    DEMO_PROJECT

--- getdata.py
+++ getdata.py
@@ -18,19 +18,3 @@ urls={
 
 
 
-# # getdata.py
-# import requests
-# def fetch_data_from_apis(api_url):
-#     response = requests.get(api_url)
-#     status_code = response.status_code
-#     if status_code == 200:
-#         return response.json()
-#     else:
-#         return None
-# Define the API URLs with names
-# api_urls = {
-#     "Councillor_API": "https://xloop-dummy.herokuapp.com/councillor",
-#     "Patient_API": "https://xloop-dummy.herokuapp.com/patient",
-#     "Patient_Councillor_API": "https://xloop-dummy.herokuapp.com/patient_councillor",
-#     "Rating_API": "https://xloop-dummy.herokuapp.com/rating"
-# }
